0a492872c8ea942f6bf65577856a1f29b39c4bf0
arumes31 <114224498+arumes31@users.noreply.github.com>
2026-02-23 10:32:15 +0100

    chore: merge alembic heads 4f9e6d8c9a3b and e7d4f6c5b2a1

---
+++ migrations/versions/49f76b520c64_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 49f76b520c64
+Revises: 4f9e6d8c9a3b, e7d4f6c5b2a1
+Create Date: 2026-02-23 10:30:38.612324
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '49f76b520c64'
+down_revision = ('4f9e6d8c9a3b', 'e7d4f6c5b2a1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
